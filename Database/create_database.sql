ba7d65cf8d5acce92e033837e9fd0624ca99273e
def <basc11111@gmail.com>
2023-12-15 19:07:17 +0300

    database file

---
+++ Database/create_database.sql
@@ -0,0 +1,16 @@
+﻿CREATE DATABASE university
+    ENCODING 'UTF-8'
+    LC_COLLATE 'ru_RU.UTF-8'
+    LC_CTYPE 'ru_RU.UTF-8';
+
+CREATE TABLE students (
+    id serial PRIMARY KEY,
+    student_name text NOT NULL,
+    study_group text NOT NULL,
+    date_of_birth date NOT NULL
+);
+
+COPY students(id, student_name, study_group, date_of_birth)
+FROM 'students.csv'
+DELIMITER ','
+CSV HEADER;
