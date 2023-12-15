a7c88b37ac9715311f277413e3354a490dd7c6f6
def <basc11111@gmail.com>
2023-12-15 20:01:46 +0300

    fix sql

--- Database/create_database.sql
+++ Database/create_database.sql
@@ -1,13 +1,13 @@
 ﻿CREATE DATABASE university;
 
-CREATE TABLE students (
+CREATE TABLE university.students (
     id serial PRIMARY KEY,
     student_name text NOT NULL,
     study_group text NOT NULL,
     date_of_birth date NOT NULL
 );
 
-COPY students(id, student_name, study_group, date_of_birth)
+COPY university.students(id, student_name, study_group, date_of_birth)
 FROM '/data/database/students.csv'
 DELIMITER ','
 CSV HEADER;
